4779ef535404aeb0ac03983958c0990e6a0ca590
Ilya Malyshev <malyshevin@mts.ru>
2025-09-22 23:40:54 +0300

    it 1.3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
 module.exports = {
   apps : [{
     name: 'backend',
@@ -7,11 +13,11 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      user : DEPLOY_USER,
+      host : DEPLOY_HOST,
+      ref  : DEPLOY_REF,
+      repo : 'https://github.com/malyshevin/nodejs-pm2-deploy',
+      path : DEPLOY_PATH,
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
